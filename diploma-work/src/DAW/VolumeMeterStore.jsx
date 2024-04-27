c7883ebe2608e128c92a2f743bb5cdad937bec13
Daniel Krzykowski <50460458+d4krzyk@users.noreply.github.com>
2024-04-27 23:22:38 +0200

    cleared

--- diploma-work/src/DAW/VolumeMeterStore.jsx
+++
@@ -1,9 +0,0 @@
-// VolumeMeterStore.js
-import {create} from 'zustand';
-
-const VolumeMeterStore = create((set) => ({
-  audio: null,
-  setAudio: (audio) => set((state) => ({ ...state, audio })),
-}));
-
-export default VolumeMeterStore;
